35bdb52ec4fb10a8909e25c0655f604b28feddaf
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-08-03 20:53:09 +0200

    added funtionality for genereting qr code with API

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
+//https://goqr.me/api/
+
 const wrapper = document.querySelector(".wrapper")
-
+qrInput = wrapper.querySelector(".form input")
 generateBtn = wrapper.querySelector(".form button")
+resetBtn = document.querySelector(".btn-reset")
+qrImg = wrapper.querySelector(".qr-code img")
 
-generateBtn.addEventListener("click", ()=>{
-    wrapper.classList.add("active")
+let qrValue;
+generateBtn.addEventListener("click", () => {
+    qrValue = qrInput.value;
+    if (!qrValue) return;
+    console.log(qrValue);
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
+
+    //
+    generateBtn.innerText = "we are genereting QR Code..."
+
+    //once Qr code img loaded
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code..."
+        console.log("reset", qrValue);
+        qrValue = ""
+    })
+
+})
+
+resetBtn.addEventListener("click", () => {
+    generateBtn.innerText = "Generate QR code!!"
+    wrapper.classList.remove("active");
+
 })
